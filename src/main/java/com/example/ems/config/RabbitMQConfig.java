38d89c9cd08c1c189ff97121d7443f9468634170
vishalkumar <nihalnishu20@gmail.com>
2026-02-08 22:17:14 +0530

    RabbitMQconfig file change

--- src/main/java/com/example/ems/config/RabbitMQConfig.java
+++ src/main/java/com/example/ems/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.example.ems.config;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,4 +43,20 @@ public class RabbitMQConfig {
     public Binding leaveBinding(Queue leaveQueue, DirectExchange notificationExchange) {
         return BindingBuilder.bind(leaveQueue).to(notificationExchange).with(LEAVE_ROUTING_KEY);
     }
+
+        @Bean
+        public MessageConverter jsonMessageConverter() {
+            return new Jackson2JsonMessageConverter();
+        }
+
+        @Bean
+        public RabbitTemplate rabbitTemplate(
+                ConnectionFactory connectionFactory,
+                MessageConverter messageConverter) {
+
+            RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+            rabbitTemplate.setMessageConverter(messageConverter);
+            return rabbitTemplate;
+        }
+
 }
